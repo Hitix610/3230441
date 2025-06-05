46fd60bab9dcecd4ec13289e03a723e27a0c8c4f
Hitix610 <johnwildervalenciagiraldo@gmail.com>
2025-06-05 15:21:20 -0500

    creacion proyecto par impar

---
+++ Conceptos de programacio/NumeroparImpar/Program.cs
@@ -0,0 +1,21 @@
+﻿do{
+    Console.Write("Ingrese un numero: ");
+    var numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("Es par");
+    }
+    else
+    {
+        Console.WriteLine("Es impar");
+    }
+}while(true)
+
+
+
+
+
+
+
+
